83197594fbbc70e3f696756958dc99cf50eed50a
Pankaj Patel <patel.pankaj@live.in>
2023-03-21 10:49:36 +0100

    updated-index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
 import StateToReducer from "./StateToReducer";
 
 const rootElement = document.getElementById("root");
@@ -9,7 +8,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
     <StateToReducer />
   </StrictMode>
 );
